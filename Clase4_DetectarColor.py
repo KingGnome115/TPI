679361e1b594a64e5f5c55ca63008f7bea340765
KingGnome115 <kevinjec115@gmail.com>
2022-02-17 08:24:48 -0600

    Añadi colores de mascara solo falta unirlos

--- Clase4_DetectarColor.py
+++ Clase4_DetectarColor.py
@@ -2,8 +2,17 @@ import cv2
 import numpy as np
 
 captura = cv2.VideoCapture(0)
-yelloBajo = np.array([0,100,5], np.uint8)
-yelloAlto = np.array([10,255,255], np.uint8)
+yelloBajo = np.array([15,100,20], np.uint8)
+yelloAlto = np.array([45,255,255], np.uint8)
+
+greenBajo = np.array([30,100,20], np.uint8)
+greenAlto = np.array([80,255,255], np.uint8)
+
+redBajo1 = np.array([0,100,5], np.uint8)
+redAlto1 = np.array([10,255,255], np.uint8)
+
+redBajo2 = np.array([175,100,20], np.uint8)
+redAlto2 = np.array([179,255,255], np.uint8)
 
 while(captura.isOpened()):
     ret, imagen = captura.read()
@@ -11,8 +20,25 @@ while(captura.isOpened()):
         frameSHV = cv2.cvtColor(imagen, cv2.COLOR_BGR2HSV) #Convertir a HSV
         maskYellow = cv2.inRange(frameSHV, yelloBajo, yelloAlto) #Crear mascara de color amarillo
         maskYellowvis = cv2.bitwise_and(imagen, imagen, mask=maskYellow) #Crear mascara de color amarillo visualizado
-        cv2.imshow('maskYellowVis', maskYellowvis)
-        cv2.imshow('maskYellow', maskYellow)
+        maskGreen = cv2.inRange(frameSHV, greenBajo, greenAlto) #Crear mascara de color verde
+        maskGreenvis = cv2.bitwise_and(imagen, imagen, mask=maskGreen) #Crear mascara de color verde visualizado
+        maskRed = cv2.inRange(frameSHV, redBajo1, redAlto1) #Crear mascara de color rojo
+        maskRedvis = cv2.bitwise_and(imagen, imagen, mask=maskRed) #Crear mascara de color rojo visualizado
+        maskRed2 = cv2.inRange(frameSHV, redBajo2, redAlto2) #Crear mascara de color rojo
+        maskRed2vid = cv2.bitwise_and(imagen, imagen, mask=maskRed2)
+        #unir mascaras
+        mask = cv2.add(maskYellow, maskGreen)
+        mask = cv2.add(mask, maskRed)
+        mask = cv2.add(mask, maskRed2)
+        maskvis = cv2.bitwise_and(imagen, imagen, mask=mask)
+
+        #cv2.imshow('maskYellowVis', maskYellowvis)
+        #cv2.imshow('maskGreenVis', maskGreenvis)
+        #cv2.imshow('maskRedVis', maskRedvis)
+        #cv2.imshow('maskRed2Vis', maskRed2vid)
+        #cv2.imshow('maskYellow', maskYellow)
+        #cv2.imshow('maskGreen', maskGreen)
+        cv2.imshow('mas', mask)
         cv2.imshow('Camara', imagen)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
