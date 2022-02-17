b383370936a14983e6fc8002eb92a90010335e78
KingGnome115 <kevinjec115@gmail.com>
2022-02-17 07:37:27 -0600

    Detectamos colores de la camara en amarillo

---
+++ Clase4_DetectarColor.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+
+captura = cv2.VideoCapture(0)
+yelloBajo = np.array([15,100,20], np.uint8)
+yelloAlto = np.array([45,255,255], np.uint8)
+
+while(captura.isOpened()):
+    ret, imagen = captura.read()
+    if ret == True:
+        frameSHV = cv2.cvtColor(imagen, cv2.COLOR_BGR2HSV) #Convertir a HSV
+        maskYellow = cv2.inRange(frameSHV, yelloBajo, yelloAlto) #Crear mascara de color amarillo
+        maskYellowvis = cv2.bitwise_and(imagen, imagen, mask=maskYellow) #Crear mascara de color amarillo visualizado
+        cv2.imshow('maskYellowVis', maskYellowvis)
+        cv2.imshow('maskYellow', maskYellow)
+        cv2.imshow('Camara', imagen)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else: break
+captura.release()
+cv2.destroyAllWindows()
