0d2f350b91166af480cd931d104a94defa1852d5
KingGnome115 <kevinjec115@gmail.com>
2022-02-17 07:41:05 -0600

    Detectamos colores de la camara en verde

--- Clase4_DetectarColor.py
+++ Clase4_DetectarColor.py
@@ -2,8 +2,8 @@ import cv2
 import numpy as np
 
 captura = cv2.VideoCapture(0)
-yelloBajo = np.array([15,100,20], np.uint8)
-yelloAlto = np.array([45,255,255], np.uint8)
+yelloBajo = np.array([45,50,65], np.uint8)
+yelloAlto = np.array([55,255,255], np.uint8)
 
 while(captura.isOpened()):
     ret, imagen = captura.read()
