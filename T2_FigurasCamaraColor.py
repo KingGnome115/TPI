38936b36e799c84c0cab3649d918f626d5630e50
KingGnome115 <kevinjec115@gmail.com>
2022-02-24 07:59:03 -0600

    En un ambiente controlado ya detecta el color

--- T2_FigurasCamaraColor.py
+++ T2_FigurasCamaraColor.py
@@ -4,48 +4,32 @@ import numpy as np
 captura = cv2.VideoCapture(0)
 
 def figColor(imageHSV):
-	#Colores a Detectar
-	#Valores de Rojo parte baja
-	redBajo1=np.array([0,100,20], np.uint8)
-	redAlto1=np.array([10,255,255], np.uint8)
-	#Valores de Rojo parte alta
-	redBajo2=np.array([175,100,20], np.uint8)
-	redAlto2=np.array([180,255,255], np.uint8)
+	yelloBajo = np.array([15,100,20], np.uint8)
+	yelloAlto = np.array([45,255,255], np.uint8)
 
-	#Valores de Naranja parte baja
+	greenBajo = np.array([30,100,20], np.uint8)
+	greenAlto = np.array([80,255,255], np.uint8)
+
+	redBajo1 = np.array([0,100,20], np.uint8)
+	redAlto1 = np.array([180,255,255], np.uint8)
+
+	redBajo2 = np.array([175,100,20], np.uint8)
+	redAlto2 = np.array([179,255,255], np.uint8)
+
+	azulBajo = np.array([100,100,115], np.uint8)
+	azulAlto = np.array([120,255,255], np.uint8)
+
 	orangeBajo1=np.array([11,100,20], np.uint8)
 	orangeAlto1=np.array([19,255,255], np.uint8)
-
-	#Valores de Amarillo parte baja
-	yellowBajo1=np.array([15,100,20], np.uint8)
-	yellowAlto1=np.array([45,255,255], np.uint8)
-
-	#Valores de Verde parte baja
-	greenBajo1=np.array([36,100,20], np.uint8)
-	greenAlto1=np.array([70,255,255], np.uint8)
-
-	#Valores de Violeta parte baja
-	violetBajo1=np.array([130,100,20], np.uint8)
-	violetAlto1=np.array([145,255,255], np.uint8)
-
-	#Valores de Rosa parte baja
-	pinkBajo1=np.array([146,100,20], np.uint8)
-	pinkAlto1=np.array([170,255,255], np.uint8)
-
-	#Valores de Azul parte baja
-	blueBajo1=np.array([100,100,20], np.uint8)
-	blueAlto1=np.array([125,255,255], np.uint8)
 
 	#se buscan los colores de la imagen de acuerdo a sus limites
 	maskRed1 = cv2.inRange(imageHSV,redBajo1,redAlto1)
 	maskRed2 = cv2.inRange(imageHSV,redBajo2,redAlto2)
 	maskRed = cv2.add(maskRed1,maskRed2)
 	maskOrange = cv2.inRange(imageHSV,orangeBajo1,orangeAlto1)
-	maskYellow = cv2.inRange(imageHSV,yellowBajo1,yellowAlto1)
-	maskGreen = cv2.inRange(imageHSV,greenBajo1,greenAlto1)
-	maskViolet = cv2.inRange(imageHSV,violetBajo1,violetAlto1)
-	maskPink = cv2.inRange(imageHSV,pinkBajo1,pinkAlto1)
-	maskBlue = cv2.inRange(imageHSV,blueBajo1,blueAlto1)
+	maskYellow = cv2.inRange(imageHSV,yelloBajo,yelloAlto)
+	maskGreen = cv2.inRange(imageHSV,greenBajo,greenAlto)
+	maskBlue = cv2.inRange(imageHSV,azulBajo,azulAlto)
 
 	#Deteccion de los colores en la imagen
 	#contornos, _ = cv2.findContours(mask,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -53,19 +37,15 @@ def figColor(imageHSV):
 	cntsOrange, _ = cv2.findContours(maskOrange,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 	cntsYellow, _ = cv2.findContours(maskYellow,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 	cntsGreen, _ = cv2.findContours(maskGreen,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-	cntsViolet, _ = cv2.findContours(maskViolet,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-	cntsPink, _ = cv2.findContours(maskPink,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 	cntsBlue, _ = cv2.findContours(maskBlue,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-	color='x'
+	color=''
 
 	#Etiquetado de color
 	if len(cntsRed)>0: color='Rojo'
 	elif len(cntsOrange)>0: color='Naranja'
 	elif len(cntsYellow)>0: color='Amarillo'
 	elif len(cntsGreen)>0: color='Verde'
-	elif len(cntsViolet)>0: color='Violeta'
-	elif len(cntsPink)>0: color='Rosa'
 	elif len(cntsBlue)>0: color='Azul'
 
 	return color
@@ -73,11 +53,11 @@ def figColor(imageHSV):
 
 def figName(contorno, width,heigth):
 	nameFig=''
-	epsilon = 0.05 * cv2.arcLength(contorno,True)#Cambiar el porcentaje de epsilon
+	epsilon = 0.01 * cv2.arcLength(contorno,True)#Cambiar el porcentaje de epsilon
 	approx= cv2.approxPolyDP(contorno,epsilon,True)
 	#Etiquetar cada figura
-	#if len(approx)==3:
-	#	nameFig ='Triangulo'
+	if len(approx)==3:
+		nameFig ='Triangulo'
 
 	if len(approx)==4:
 		aspect_ratio = float(width)/heigth
@@ -86,14 +66,14 @@ def figName(contorno, width,heigth):
 		else:
 			nameFig ='Rectangulo'
 
-	#if len(approx)==5:
-	#	nameFig ='Pentagono'
+	if len(approx)==5:
+		nameFig ='Pentagono'
 
-	#if len(approx)==6:
-	#	nameFig ='Hexagono'
+	if len(approx)==6:
+		nameFig ='Hexagono'
 
-	#if len(approx)>10:
-	#	nameFig ='Circulo'
+	if len(approx)>10:
+		nameFig ='Circulo'
 
 	return nameFig
 
@@ -104,7 +84,7 @@ while(captura.isOpened()):
         #tomar un frame con los colores originales
         frame = imagen.copy()
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        canny = cv2.Canny(gray,100,150)
+        canny = cv2.Canny(gray,10,150)
         canny = cv2.dilate(canny, None, iterations=1)
         canny = cv2.erode(canny, None, iterations=1)
         contornos, _ = cv2.findContours(canny,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -116,10 +96,12 @@ while(captura.isOpened()):
             imAux = cv2.drawContours(imAux, [cnt], -1, 255, -1)
             maskHSV = cv2.bitwise_and(imageHSV, imageHSV, mask=imAux)
             figAll = figName(cnt, w, h) + ' ' + figColor(maskHSV)
-            cv2.putText(frame, figAll, (x, y), 1,1, (0, 255, 0), 1)
+            cv2.putText(frame,figAll,(x,y-5),1,1,(0,255,0),1)
+
 
         cv2.imshow('Camara',frame)
         cv2.imshow('Canny',canny)
+
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     else:
